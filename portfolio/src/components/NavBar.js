a60914d185cc64f76e3f3584a79e087866f9e050
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-03 21:49:26 -0400

    add Navbar

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -3,11 +3,57 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return(
-        <h1>Navbar</h1>
+        <Navbar
+    //   expanded={expand}
+    //   fixed="top"
+    //   expand="md"
+    //   className={navColour ? "sticky" : "navbar"}
+    >
+      <Container>
+        <Navbar.Brand href="/">
+          Brands Here!!
+        </Navbar.Brand>
+        <Navbar.Toggle
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto" defaultActiveKey="#home">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/" >
+                <i className="fas fa-home"></i> Home
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/about"
+                
+              >
+                <i className="far fa-user"></i> About
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/project"
+               
+              >
+                <i className="fab fa-codepen"></i> Projects
+              </Nav.Link>
+            </Nav.Item>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     )
 }
 
