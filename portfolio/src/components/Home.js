c950bed69b20badaf27698ce555fa0f4a8862d0d
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-05 23:34:28 -0400

    add homepage

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Home = () => {
     return(
-        <h1>Home</h1>
+        <section>
+        <Container fluid className="home-section" id="home">
+         
+          <Container className="home-content">
+            <Row>
+              <Col md={7} className="home-header">
+                <h1 style={{ padding: 100 }} className="heading">
+                  Hello! 
+                </h1>
+  
+                <h1 className="heading-name">
+                  I'M
+                  <strong className="main-name"> CHAYANIT MCCORMICK</strong>
+                </h1>
+  
+                <div style={{ padding: 50, textAlign: "left" }}>
+                
+                </div>
+              </Col>
+  
+              <Col md={5} style={{ paddingBottom: 20 }}>
+                <h3 style={{ padding: 150 }}>Pic goes here</h3>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      </section>
     )
 }
 
--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -3,23 +3,14 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return(
-        <Navbar
-    //   expanded={expand}
-    //   fixed="top"
-    //   expand="md"
-    //   className={navColour ? "sticky" : "navbar"}
-    >
+  return (
+    <Navbar>
       <Container>
-        <Navbar.Brand href="/">
-          Brands Here!!
-        </Navbar.Brand>
-        <Navbar.Toggle
-        >
+        <Navbar.Brand href="/">Brands Here!!</Navbar.Brand>
+        <Navbar.Toggle>
           <span></span>
           <span></span>
           <span></span>
@@ -27,34 +18,25 @@ const NavBar = () => {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto" defaultActiveKey="#home">
             <Nav.Item>
-              <Nav.Link as={Link} to="/" >
+              <Nav.Link as={Link} to="/">
                 <i className="fas fa-home"></i> Home
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/about"
-                
-              >
+              <Nav.Link as={Link} to="/about">
                 <i className="far fa-user"></i> About
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/project"
-               
-              >
+              <Nav.Link as={Link} to="/project">
                 <i className="fab fa-codepen"></i> Projects
               </Nav.Link>
             </Nav.Item>
-            
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
