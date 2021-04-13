151994dad48f421ed77005ab4216642daabc9979
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-13 14:23:47 -0400

    add projects

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -4,12 +4,13 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
+import "./navbar.css"
 
 const NavBar = () => {
   return (
-    <Navbar collapseOnSelect expand="lg">
+    <Navbar collapseOnSelect expand="lg" className="navbar-head sticky-top">
       <Container>
-        <Navbar.Brand href="/">Brands Here!!</Navbar.Brand>
+        <Navbar.Brand className="navbar" href="/">Brands Here!!</Navbar.Brand>
         <Navbar.Toggle  area-controls="responsive-navbar-nav" /> 
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto" defaultActiveKey="#home">
@@ -18,14 +19,16 @@ const NavBar = () => {
                 <i className="fas fa-home"></i> Home
               </Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as={Link} to="/about">
-                <i className="far fa-user"></i> About
-              </Nav.Link>
-            </Nav.Item>
+            
             <Nav.Item>
               <Nav.Link as={Link} to="/project">
                 <i className="fab fa-codepen"></i> Projects
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link as={Link} to="/about">
+                <i className="far fa-user"></i> About
               </Nav.Link>
             </Nav.Item>
           </Nav>
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,14 +1,32 @@
 import React from "react";
+import ReactPlayer from 'react-player'
 import './project.css'
 
 const Projects = () => {
     return(
         <div className="work">          
             <div className="card project">
-                project
+                <h6>Project Name: Bite Bank</h6>
+                <h6>Client side: React, Redux, and Thunk</h6>
+                <h6>Server side: Ruby on Rails</h6>
+                <h6>Live app: https://bite-bank.netlify.app/ </h6>
+                <div style={{ width: "auto", height: "20rem" }}>
+                <ReactPlayer url='https://youtu.be/mVHMsb0lXLw' 
+                width="100%"
+                height="100%"
+                />
+                </div> 
             </div>
             <div className="card project">
-                project
+                <h6>Project Name: Pair It Up!</h6>
+                <h6>Client side: Javascript</h6>
+                <h6>Server side: Ruby on Rails</h6>
+                <div style={{ width: "auto", height: "20rem" }}>
+                <ReactPlayer url='https://youtu.be/M26lT_x6ADs' 
+                width="100%"
+                height="100%"
+                />
+                </div> 
             </div>
             <div className="card project">
                 project
