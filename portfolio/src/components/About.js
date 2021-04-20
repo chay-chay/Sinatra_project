948b3cf68713567d21f07d33bb7d22f2b5c04079
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-20 12:40:16 -0400

    add about section

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -4,7 +4,8 @@ import "./about.css"
 const About = () => {
     return(
         <div id="about">
-        <h1>About</h1>
+            <h1 style={{ padding: 20, textAlign: "center" }}>About</h1>
+            <p>Full stack software developer originally from Thailand with a passion to learn new technologies. Experienced in JavaScript and Ruby-based programming with a background in International Business Management. Strong problem-solving and organizational skills useful in any multidisciplinary setting. Effective in a team atmosphere with thoughtful attention to communication.</p>
         </div>
     )
 }
