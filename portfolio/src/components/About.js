5164e01992d34f71eb9f7f6b6860a9d8c4be5f0f
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-22 14:01:02 -0400

    get rid of avatar

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./about.css"
-import avatar from "../images/avatar.png"
+
 
 const About = () => {
     return(
         <div id="about">
             <h1 style={{ padding: 20, textAlign: "center" }}>About</h1>
-            <p>Full stack software developer originally from Thailand with a passion to learn new technologies. Experienced in JavaScript and Ruby-based programming with a background in International Business Management. Strong problem-solving and organizational skills useful in any multidisciplinary setting. Effective in a team atmosphere with thoughtful attention to communication.</p>
-            <img src={avatar} alt="avatar" />
+            <div className="about-me">
+                <p>Full stack software developer originally from Thailand with a passion to learn new technologies. Experienced in JavaScript and Ruby-based programming with a background in International Business Management. Strong problem-solving and organizational skills useful in any multidisciplinary setting. Effective in a team atmosphere with thoughtful attention to communication.</p>
+              
+            </div>
+            
         </div>
     )
 }
