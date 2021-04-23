e4b16b343f6f4c1808e05c9b9d7ee74d405d283a
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-23 12:12:38 -0400

    deploy on github

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./about.css"
+
+
+const About = () => {
+    return(
+        <div id="about">
+            <h1 style={{ padding: 20, textAlign: "center" }}>About</h1>
+            <div className="about-me">
+                <p>Full stack software developer originally from Thailand with a passion to learn new technologies. Experienced in JavaScript and Ruby-based programming with a background in International Business Management. Strong problem-solving and organizational skills useful in any multidisciplinary setting. Effective in a team atmosphere with thoughtful attention to communication.</p>
+              
+            </div>
+            
+        </div>
+    )
+}
+
+export default About
